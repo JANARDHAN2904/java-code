b78366e7063b357e07f892c50b9be2116c3b01d9
JANARDHAN2904 <mjanardhan630@gmail.com>
2021-03-13 12:24:01 +0530

    initial commit for first time.

---
+++ Larger.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Larger{
+	public static void main(String args[])
+	{
+		Scanner s = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int n = s.nextInt();
+		if(n==5)
+			System .out.println("equal to 5");
+		else if(n<5)
+			System .out.println("lesser than 5");
+		else
+			System .out.println("Grater than 5");
+
+	}
+	}
